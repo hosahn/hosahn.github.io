5f0e3c2fec1b7edee8839b761cb3d34a1640c860
Hosan_Lee <hosahn13@gmail.com>
2022-07-15 00:30:19 +0900

    Fixed .env

--- back/src/router/loginRouter.js
+++ back/src/router/loginRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const loginRouter = express.Router();
 
-loginRouter.post("/", (req, res) => {});
+loginRouter.post("/", (req, res) => {
+  res.send("Logged in");
+});
 
 module.exports = loginRouter;
