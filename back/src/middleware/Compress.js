4866e7e936c3f04f053887ac9b24ae41baa9f6dc
Hosan_Lee <hosahn13@gmail.com>
2022-07-17 00:26:41 +0900

    Initial commit

--- back/src/middleware/Compress.js
+++
@@ -1,8 +0,0 @@
-function shouldCompress(req, res) {
-  if (req.headers["x-no-compression"]) {
-    return false;
-  }
-  return compression.filter(req, res);
-}
-
-module.exports = shouldCompress;
--- back/src/router/loginRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const loginRouter = express.Router();
-
-loginRouter.post("/", (req, res) => {
-  res.send("Logged in");
-});
-
-module.exports = loginRouter;
