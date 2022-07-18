5f3790a0f96d38c9731731524a30a9ecf7627ad9
Hosan_Lee <hosahn13@gmail.com>
2022-07-19 00:42:04 +0900

    Added Backend Server

---
+++ back/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const helmet = require("helmet");
+const app = express();
+const rateLimit = require("express-rate-limit");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000, // limit each IP to 100 requests per windowMs
+  delayMs: 100, // disable delaying — full speed until the max limit is reached
+});
+
+const mailer = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+const address = "hosahn13@gmail.com";
+
+app.use(helmet());
+app.use(limiter);
+app.post("/contact", (req, res) => {
+  try {
+    mailer.sendMail(
+      {
+        from: req.body.from,
+        to: [address],
+        subject: req.body.subject || "[No subject]",
+        html: req.body.message || "[No message]",
+      },
+      function (err, info) {
+        if (err) return res.send(false);
+        res.send(true);
+      }
+    );
+  } catch (e) {
+    res.send(false);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("App Started in Port 3000");
+});
