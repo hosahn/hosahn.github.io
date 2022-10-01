e977931682333144d42000b6b84eadb55a7b95ae
Hosan Lee <hosahn13@gmail.com>
2022-10-01 22:50:17 +0200

    Created Next&Previous Post buttons

--- blog/src/components/Post/PostNextBefore.tsx
+++ blog/src/components/Post/PostNextBefore.tsx
@@ -1,5 +1,17 @@
 import React, { FunctionComponent} from 'react'
+import { HighLightOuter } from 'components/Common/Nav'
+import { Btn } from 'components/Common/Nav'
+import styled from '@emotion/styled'
 
+
+export const BtnDiv1 = styled.div`
+    width : 700px;
+    height : 30px;
+    display: flex;
+    justify-content: space-between;
+    margin : auto;
+
+`
 export type BeforeAfterType = {
     before : string,
     after : string
@@ -9,12 +21,20 @@ export type BeforeAfterType = {
 const RelatedList: FunctionComponent<BeforeAfterType> = function ({
   before, after
 }) {
-    const beforeString = `https://hosahn.github.io.${before}`
-    const afterString = `https://hosahn.github.io.${after}`
+    const FixedBefore = before.replace(/\./g, "").substring(2);
+    const FixedAfter = after.replace(/\./g, "").substring(2);
+    const beforeString = `https://hosahn.github.io/${FixedBefore}/`
+    const afterString = `https://hosahn.github.io/${FixedAfter}/`
   return (
     <>
-    <a href={beforeString}>이전 글</a>
-    <a href={afterString}>다음 글</a>
+    <BtnDiv1>
+    <Btn>
+    <HighLightOuter href = {beforeString}>이전 글</HighLightOuter>
+    </Btn>
+    <Btn>
+    <HighLightOuter href = {afterString}>다음 글</HighLightOuter>
+    </Btn>
+    </BtnDiv1>
     </>
   )
 }
